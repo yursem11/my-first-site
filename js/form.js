9fdd3b68b818a2b49bc472e8088196edac300ce3
Yurii <yursem@meta.ua>
2016-10-17 19:36:09 +0000

    add new files

--- js/form.js
+++ js/form.js
@@ -6,6 +6,7 @@ $(function () {
             method: "POST",
             data: {
                 message: $('#name').val(),
+                email: $('#email').val(),
                 text: $('#text').val()
             },
             dataType: "json"
@@ -15,4 +16,35 @@ $(function () {
             $('form').html(xhr.statusText);
         });        
     })
+});
+
+
+function initMap() {
+     var Bohorodchany = {
+         lat: 48.813002,
+         lng: 24.532259
+     };
+     var map = new google.maps.Map(document.getElementById('map'), {
+         zoom: 10,
+         center: Bohorodchany
+     });
+     var marker = new google.maps.Marker({
+         position: Bohorodchany,
+         map: map
+     });
+ }
+ 
+ 
+ $(document).ready(function () {
+
+    $('.btn-vertical-slider').on('click', function () {
+        
+        if ($(this).attr('data-slide') == 'next') {
+            $('#myCarousel').carousel('next');
+        }
+        if ($(this).attr('data-slide') == 'prev') {
+            $('#myCarousel').carousel('prev')
+        }
+
+    });
 });
